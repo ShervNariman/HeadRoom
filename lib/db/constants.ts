79dfc229bf0960a1d661781cd133c35fa24969f6
Sherv Nariman <prosherv@hotmail.com>
2026-07-11 20:55:16 -0400

    Define the production default workspace

---
+++ lib/db/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_WORKSPACE_ID = "00000000-0000-4000-8000-000000000001";
+export const DEFAULT_WORKSPACE_SLUG = "default";
+export const DEFAULT_WORKSPACE_NAME = "Default workspace";
